993c5f8bfa18494cfe8bdb7db9a8baeaaa898049
bijum777 <bijum777@gmail.com>
2025-11-22 17:59:23 +0300

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
    // apiBaseUrl: 'http://localhost:5000',
   apiBaseUrl: 'https://coverletterauthserviceapi-production.up.railway.app',
-  resumeUploadApiUrl: 'coverletterresumeserviceapi-production.up.railway.app',
+  resumeUploadApiUrl: 'https://coverletterresumeserviceapi-production.up.railway.app',
   //resumeUploadApiUrl: 'http://localhost:3000'
 };
